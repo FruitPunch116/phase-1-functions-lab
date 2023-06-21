d1b71265be365b63eaedd2185f3e77ad7e2de12a
Angel Figueroa <gabrielfigue4120@gmail.com>
2023-06-21 10:30:38 -0400

    change if statement

--- index.js
+++ index.js
@@ -4,46 +4,40 @@ function distanceFromHqInBlocks (blocks) {
     const headquarterLocation = 42
     if (blocks < headquarterLocation) {
         return  headquarterLocation - blocks
-    }
-    
-    if (blocks > headquarterLocation) {
+    } else if (blocks > headquarterLocation) {
         return blocks - headquarterLocation 
     }
     
 }
 
-console.log(`This is the blocks distance from the 42 street ${distanceFromHqInBlocks(43)}`)
-console.log(`This is the blocks distance from the 42 street ${distanceFromHqInBlocks(50)}`)
-console.log(`This is the blocks distance from the 42 street ${distanceFromHqInBlocks(34)}`)
+// console.log(`This is the blocks from the 42nd street to the 43rd street: ${distanceFromHqInBlocks(43)}`)
+// console.log(`This is the blocks from the 42nd street to the 50th street: ${distanceFromHqInBlocks(50)}`)
+// console.log(`This is the blocks from the 42nd street to the 34th street: ${distanceFromHqInBlocks(34)}`)
 
 function distanceFromHqInFeet (feet) {
     const headquarterLocation = 42
     if (feet > headquarterLocation) {
         return (feet - headquarterLocation) * 264
-    }
-
-    if (feet < headquarterLocation) {
+    } else if (feet < headquarterLocation) {
         return (headquarterLocation - feet) * 264 
     }
 }
 
-console.log(`this is feet distance ${distanceFromHqInFeet(43)}`)
-console.log(`this is feet distance ${distanceFromHqInFeet(50)}`)
-console.log(`this is feet distance ${distanceFromHqInFeet(34)}`)
+// console.log(`This is feet distance from the Headquarters: ${distanceFromHqInFeet(43)}`)
+// console.log(`This is feet distance from the Headquarters: ${distanceFromHqInFeet(50)}`)
+// console.log(`This is feet distance from the Headquarters: ${distanceFromHqInFeet(34)}`)
 
 function distanceTravelledInFeet (currentLoc, destination) {
     if (currentLoc > destination) {
         return (currentLoc - destination) * 264
-    }
-
-    if (currentLoc < destination) {
+    } else if (currentLoc < destination) {
         return (destination - currentLoc) * 264
     }
 }
 
-console.log(`This dis distance and destination ${distanceTravelledInFeet(43,48)}`)
-console.log(`This dis distance and destination ${distanceTravelledInFeet(50,60)}`)
-console.log(`This dis distance and destination ${distanceTravelledInFeet(34,28)}`)
+// console.log(`This is distance to destination: ${distanceTravelledInFeet(43,48)}`)
+// console.log(`This is distance to destination: ${distanceTravelledInFeet(50,60)}`)
+// console.log(`This is distance to destination: ${distanceTravelledInFeet(34,28)}`)
 
 function calculatesFarePrice (currentLoc, destination) {
     let distance = distanceTravelledInFeet(currentLoc, destination)
@@ -58,7 +52,7 @@ function calculatesFarePrice (currentLoc, destination) {
     }
 }
 
-console.log(`This is fare price ${calculatesFarePrice(43,44)}`)
-console.log(`This is fare price ${calculatesFarePrice(34,32)}`)
-console.log(`This is fare price ${calculatesFarePrice(50,58)}`)
-console.log(`This is fare price ${calculatesFarePrice(34,24)}`)
+// console.log(`This is your fare price: ${calculatesFarePrice(43,44)}`)
+// console.log(`This is your fare price: ${calculatesFarePrice(34,32)}`)
+// console.log(`This is your fare price: ${calculatesFarePrice(50,58)}`)
+// console.log(`This is your fare price: ${calculatesFarePrice(34,24)}`)
